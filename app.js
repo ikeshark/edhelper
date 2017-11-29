9f9907b979cd0528df5f5020f395d98ecae0c960
Michael Shank <mike.shank@gmail.com>
2017-11-29 12:13:07 -0500

    another go at fullscreen

--- app.js
+++ app.js
@@ -289,20 +289,21 @@ document.getElementById("changeColors").addEventListener("click", function() {
   });
 });
 // mozilla full screen function
-// function toggleFullScreen() {
-//   var doc = window.document;
-//   var docEl = doc.documentElement;
-//
-//   var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-//   var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-//
-//   if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-//     requestFullScreen.call(docEl);
-//   }
-//   else {
-//     cancelFullScreen.call(doc);
-//   }
-// };
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+};
+document.getElementById("fullButt").addEventListener("click", toggleFullScreen);
 displayBoard();
 
 // to do:
